a6d19fc481d123caedabc3698afde3c430e00b46
slemeshaev <lemeshaevs@gmail.com>
2020-12-09 18:22:16 +0300

    Files for project

---
+++ FlashLight/FlashLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashLight
+//
+//  Created by Станислав Лемешаев on 09.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
