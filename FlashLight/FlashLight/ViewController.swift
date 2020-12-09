6330fb3eb000339af4e1e85e75ccba0b8ef610eb
slemeshaev <lemeshaevs@gmail.com>
2020-12-09 18:49:23 +0300

    Setting up

--- FlashLight/FlashLight/ViewController.swift
+++ FlashLight/FlashLight/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var result: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
